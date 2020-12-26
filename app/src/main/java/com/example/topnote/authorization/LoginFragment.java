335c02a0d105e157269c391b57392fc348812265
vishvajeet590 <vishvajeet878@gmail.com>
2020-12-26 14:32:15 +0530

    STarted

---
+++ app/src/main/java/com/example/topnote/authorization/LoginFragment.java
@@ -0,0 +1,15 @@
+package com.example.topnote.authorization;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.topnote.R;
+
+public class LoginFragment extends Fragment {
+
+}
